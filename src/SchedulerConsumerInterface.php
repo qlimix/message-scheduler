1a14698b414908b553787d0589cc1a1ac286690f
frank <frank@qlimix.net>
2018-09-05 19:43:54 +0200

    initial setup queue scheduler

---
+++ src/SchedulerConsumerInterface.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace Qlimix\Queue\Scheduler;
+
+use Qlimix\Queue\Scheduler\Exception\SchedulerConsumeException;
+
+interface SchedulerConsumerInterface
+{
+    /**
+     * @return ScheduledMessage[]
+     *
+     * @throws SchedulerConsumeException
+     */
+    public function consume(): array;
+}
