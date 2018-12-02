27c05ab1188f9857ab7c53fe414134e11213da07
frank <frank@qlimix.net>
2018-12-02 19:05:26 +0100

    pass along the acknowledge interface for the process interface

--- src/Processor/SchedulerProcessorInterface.php
+++ src/Processor/SchedulerProcessorInterface.php
@@ -2,15 +2,16 @@
 
 namespace Qlimix\Message\Scheduler\Processor;
 
-use Qlimix\Message\Scheduler\Consumer\ScheduledMessage;
+use Qlimix\Message\Scheduler\Consumer\SchedulerAcknowledgeInterface;
 use Qlimix\Message\Scheduler\Processor\Exception\SchedulerProcessException;
 
 interface SchedulerProcessorInterface
 {
     /**
-     * @param ScheduledMessage[] $messages
+     * @param array $messages
+     * @param SchedulerAcknowledgeInterface $acknowledge
      *
      * @throws SchedulerProcessException
      */
-    public function process(array $messages): void;
+    public function process(array $messages, SchedulerAcknowledgeInterface $acknowledge): void;
 }
