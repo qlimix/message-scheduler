19aaf4a214fb9b1c1a9aed5f33d90f13db5612a4
frank <frank@qlimix.net>
2018-12-03 19:37:44 +0100

    process does all the work now, consume and acknowledge

--- src/Processor/SchedulerProcessorInterface.php
+++ src/Processor/SchedulerProcessorInterface.php
@@ -2,15 +2,12 @@
 
 namespace Qlimix\Message\Scheduler\Processor;
 
-use Qlimix\Message\Scheduler\Consumer\ScheduledMessage;
 use Qlimix\Message\Scheduler\Processor\Exception\SchedulerProcessException;
 
 interface SchedulerProcessorInterface
 {
     /**
-     * @param ScheduledMessage[] $messages
-     *
      * @throws SchedulerProcessException
      */
-    public function process(array $messages): void;
+    public function process(): void;
 }
